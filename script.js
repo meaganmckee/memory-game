7dd175f980d66c3d5f5c8fc9be12ea0d5ca8f54f
Chris Calleja <callejac1@gmail.com>
2022-07-08 14:39:12 -0400

    12 cards!

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 "use strict";
 
-const card = document.getElementById("card");
-
-card.addEventListener("click", flipCard);
-
-function flipCard() {
-  card.classList.toggle("flipCard");
+const card = document.querySelectorAll(".card");
+console.log(card);
+for (let i = 0; i < 12; i++) {
+  card[i].addEventListener("click", flipCard);
+  function flipCard() {
+    card[i].classList.toggle("flipCard");
+  }
 }
